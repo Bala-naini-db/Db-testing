807f3b84a434a6e3de11abcc77b422cb60b13973
Bala-naini-db <87867804+Bala-naini-db@users.noreply.github.com>
2021-07-26 19:04:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "google_compute_network" "vpc" {
 
 // Create Subnet
 resource "google_compute_subnetwork" "subnet" {
- name          = var.subnet-name
+ name          = var.subnet_name
  ip_cidr_range = var.subnet_cidr
  network       = var.vpc_network_name
  depends_on    = ["google_compute_network.vpc"]
