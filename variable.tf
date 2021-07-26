52127e097968b347ee50e09a5c31facdc1e4cfdd
Bala-naini-db <87867804+Bala-naini-db@users.noreply.github.com>
2021-07-26 19:00:43 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  type    = "string"
+  default = "testvpc01"
+
+}
+
+variable "subnet_name" {
+  type    = "string"
+  default = "testsubnet01"
+}
+
+
+variable "subnet_cidr" {
+  type    = "string"
+  default = "10.10.0.0/24"
+
+}
+
+variable "vpc_network_name" {
+  type    = "string"
+  default = "testnetwork"
+
+}
+
+variable "region" {
+  type    = "string"
+  default = "us-central1"
+
+}
